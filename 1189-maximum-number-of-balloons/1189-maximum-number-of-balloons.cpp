25c5edb4cf695d03661e77d020cd4dd04fd31c05
SAURABH GOND <gondsaurabh722@gmail.com>
2026-06-22 10:29:44 +0530

    Time: 0 ms (100%), Space: 9 MB (39.61%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+
+        unordered_map<char,int>freq;
+
+        for(auto ch: text){
+            freq[ch]++;
+        }
+
+        return min({freq['b'],freq['a'],freq['l']/2,freq['o']/2,freq['n']});
+        
+    }
+};
