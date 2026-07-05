3756ed384433f3e4f70e11dd08ed1636c55a9e35
SAURABH GOND <gondsaurabh722@gmail.com>
2026-07-06 00:18:50 +0530

    Time: 2 ms (14.13%), Space: 10.6 MB (66.68%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+
+        unordered_map<int,int>mp;
+
+        for(int x : nums){
+            mp[x]++;
+        }
+        int sum =0;
+        for(int i =0;i<nums.size();i++){
+            if(mp[nums[i]] ==1){
+                sum += nums[i];
+            }
+        }
+        return sum;
+        
+    }
+};
