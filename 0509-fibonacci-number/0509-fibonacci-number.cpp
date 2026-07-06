f4086acddbb667dedc818b1f09b502c2db356b59
SAURABH GOND <gondsaurabh722@gmail.com>
2026-07-06 18:26:50 +0530

    Time: 3 ms (65.75%), Space: 8.1 MB (18.93%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        else{
-            return fib(n-1) + fib(n-2);
+    // dp solution
+    int solve(int n, vector<int>&dp){
+        if(n<=1){
+            return n;
         }
+        // step 3;
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        //step2
+        dp[n] = solve(n-1,dp) + solve(n-2,dp);
+        return dp[n];
+    }
+    int fib(int n) {
+
+        vector<int>dp(n+1,-1);
+
+        return solve(n,dp);
+        
     }
 };
