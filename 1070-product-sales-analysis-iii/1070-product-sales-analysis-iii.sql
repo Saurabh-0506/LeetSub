da6e5b8772aabcbf9229f5cb75c8dac482514366
SAURABH GOND <gondsaurabh722@gmail.com>
2026-08-11 20:53:51 +0530

    Time: 731 ms (63.23%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,7 +1,12 @@
 
-SELECT s1.product_id,s1.year AS first_year,s1.quantity,s1.price
-FROM Sales s1
-LEFT JOIN Sales s2
-ON s1.product_id = s2.product_id
-AND s1.year > s2.year
-WHERE s2.product_id IS NULL;
+-- SELECT s1.product_id,s1.year AS first_year,s1.quantity,s1.price
+-- FROM Sales s1
+-- LEFT JOIN Sales s2
+-- ON s1.product_id = s2.product_id
+-- AND s1.year > s2.year
+-- WHERE s2.product_id IS NULL;
+SELECT product_id,year as first_year,quantity,price
+From Sales
+where(product_id,year) IN (Select product_id,MIN(year) as f_year
+from Sales
+group by product_id)
